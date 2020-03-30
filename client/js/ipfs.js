30b0d7c23f9a368d65fec7ca07364f6d6310f6b9
Tezan <tezansahu@gmail.com>
2020-03-30 10:18:36 +0530

    ipfs validation; pics changed

--- client/js/ipfs.js
+++ client/js/ipfs.js
@@ -1,19 +1,27 @@
 function upload() {
+    document.getElementById("ipfs_err").style.display = "none";
     const reader = new FileReader();
     reader.onloadend = function() {
-        const ipfs = window.IpfsApi('localhost', 5003) // Connect to IPFS
-        const buf = buffer.Buffer(reader.result) // Convert data into buffer
-        ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
-            if(err) {
-                console.error(err)
-                return
-            }
+        try{
+            const ipfs = window.IpfsApi('localhost', 5003) // Connect to IPFS
+            const buf = buffer.Buffer(reader.result) // Convert data into buffer
+            ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
+                if(err) {
+                    console.error(err)
+                    document.getElementById("ipfs_err").style.display = "block";
+                    return
+                }
             let url = `https://ipfs.io/ipfs/${result[0].hash}`
             console.log(`Url --> ${url}`)
             document.getElementById("ipfs_hash").value = result[0].hash
             document.getElementById("ipfs_url").href= url
+            })
+        }
+        catch{
+            document.getElementById("ipfs_err").style.display = "block";
+        }
+        
             // document.getElementById("output").src = url
-        })
     }
     const photo = document.getElementById("service_prescription_file");
     reader.readAsArrayBuffer(photo.files[0]); // Read Provided File
